192f092901169aff8098f0a176f3dde2a33c3ba5
Dake Zhao <dzhao@wi-fi.org>
2018-02-23 00:19:25 -0800

    Update blog

--- blogproject/blog/models.py
+++ blogproject/blog/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.urls import reverse
 
 class Category(models.Model):
     """
@@ -64,4 +64,10 @@ class Post(models.Model):
     author = models.ForeignKey(User)
 
     def __str__(self):
-        return self.title                      
+        return self.title     
+
+    # 自定义 get_absolute_url 方法
+    # 记得从 django.urls 中导入 reverse 函数
+    def get_absolute_url(self):
+        return reverse('blog:detail', kwargs={'pk': self.pk})       
+          
